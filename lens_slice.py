5e586b286bebd871346915464f895ab2c0870510
n-crandall <99920990+n-crandall@users.noreply.github.com>
2022-07-09 23:52:24 -0600

    using count method on prices

--- lens_slice.py
+++ lens_slice.py
@@ -1,3 +1,7 @@
 toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
 
 prices = [2, 6, 1, 3, 2, 7, 2]
+
+#counting $2 slices
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
